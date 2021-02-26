8afc9d62b1201c8579e9baa50140800647df1a0d
amirelrahep <amirelrahep@gmail.com>
2021-02-26 15:04:38 -0500

    Added event handler for the Play button

--- app/src/main/java/com/wsu/towerdefense/MainActivity.java
+++ app/src/main/java/com/wsu/towerdefense/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,9 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // go to game activity until menu is made
-        //Intent intent = new Intent(this, GameActivity.class);
-        //startActivity(intent);
     }
+
+
+    /**
+     * This method is for when the play button is clicked.
+     *
+     * @param view view
+     */
+    public void playButtonClicked(View view) {
+        // go to game activity until menu is made
+        Intent intent = new Intent(this, GameActivity.class);
+        startActivity(intent);
+    }
+
 }
