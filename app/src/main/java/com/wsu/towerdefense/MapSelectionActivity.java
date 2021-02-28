fa2a7b0d1670e9e74fa965b806ad0a3ec2c79710
amirelrahep <amirelrahep@gmail.com>
2021-02-27 19:13:41 -0500

    Added event handler for the play button

--- app/src/main/java/com/wsu/towerdefense/MapSelectionActivity.java
+++ app/src/main/java/com/wsu/towerdefense/MapSelectionActivity.java
@@ -2,7 +2,9 @@ package com.wsu.towerdefense;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MapSelectionActivity extends AppCompatActivity {
 
@@ -11,4 +13,17 @@ public class MapSelectionActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map_selection);
     }
+
+
+    /**
+     * This method is for when the play button is clicked. When the play button clicked, it
+     * goes to the GameActivity.
+     *
+     * @param view view
+     */
+    public void btnPlayClicked(View view) {
+        Intent intent = new Intent(this, GameActivity.class);
+        startActivity(intent);
+    }
+
 }
