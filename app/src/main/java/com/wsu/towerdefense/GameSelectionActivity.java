3b5f916777e2035f8abe4066a633bb7a7b091932
amirelrahep <amirelrahep@gmail.com>
2021-03-01 12:06:02 -0500

    Added event handler for the new game button

--- app/src/main/java/com/wsu/towerdefense/GameSelectionActivity.java
+++ app/src/main/java/com/wsu/towerdefense/GameSelectionActivity.java
@@ -2,7 +2,9 @@ package com.wsu.towerdefense;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class GameSelectionActivity extends AppCompatActivity {
 
@@ -11,4 +13,17 @@ public class GameSelectionActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_selection);
     }
+
+
+    /**
+     * This method is for when the nee game button is clicked. When the new game button clicked, it
+     * goes to the MapSelectionActivity.
+     *
+     * @param view view
+     */
+    public void btnNewGameClicked(View view) {
+        Intent intent = new Intent(this, MapSelectionActivity.class);
+        startActivity(intent);
+    }
+
 }
