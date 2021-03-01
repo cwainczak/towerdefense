7c860a40073f7baf84caad1b33bc5b0f83541a4a
amirelrahep <amirelrahep@gmail.com>
2021-03-01 17:53:07 -0500

    Updated settings activity layout

--- app/src/main/java/com/wsu/towerdefense/SettingsActivity.java
+++ app/src/main/java/com/wsu/towerdefense/SettingsActivity.java
@@ -3,12 +3,23 @@ package com.wsu.towerdefense;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.SeekBar;
 
 public class SettingsActivity extends AppCompatActivity {
+
+    SeekBar sb_music;
+    SeekBar sb_soundFx;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        sb_music = findViewById(R.id.sb_music);
+        sb_soundFx = findViewById(R.id.sb_soundFx);
+
+        sb_music.setProgress(100);
+        sb_soundFx.setProgress(100);
     }
 }
