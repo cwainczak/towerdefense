911c4db0a78c59dbe0ed4b2685066bf041660084
amirelrahep <amirelrahep@gmail.com>
2021-03-01 20:11:07 -0500

    Added event handler for back button

--- app/src/main/java/com/wsu/towerdefense/SettingsActivity.java
+++ app/src/main/java/com/wsu/towerdefense/SettingsActivity.java
@@ -2,7 +2,9 @@ package com.wsu.towerdefense;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.SeekBar;
 
 public class SettingsActivity extends AppCompatActivity {
@@ -21,5 +23,17 @@ public class SettingsActivity extends AppCompatActivity {
 
         sb_music.setProgress(100);
         sb_soundFx.setProgress(100);
+    }
+
+
+    /**
+     * This method is for when the back button is clicked. When the back button is clicked, it
+     * goes to the MainActivity.
+     *
+     * @param view view
+     */
+    public void btnBackClicked(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
