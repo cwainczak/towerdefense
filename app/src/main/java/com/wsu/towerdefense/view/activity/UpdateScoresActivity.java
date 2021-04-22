d43d3b78eb04f6777d303d4c550a7d9060a43021
Cooper Wainczak <gainczak@gmail.com>
2021-04-22 02:15:40 -0400

    Added Win Screen

--- app/src/main/java/com/wsu/towerdefense/view/activity/UpdateScoresActivity.java
+++ app/src/main/java/com/wsu/towerdefense/view/activity/UpdateScoresActivity.java
@@ -4,7 +4,9 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -13,10 +15,15 @@ import com.mysql.jdbc.StringUtils;
 import com.wsu.towerdefense.Model.Highscores.DBTools;
 import com.wsu.towerdefense.R;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class UpdateScoresActivity extends Activity {
 
     private String playerUsername;
     private int playerScore;
+
+    private static volatile boolean showOverlay = true;
 
     private final int MAX_USERNAME_LENGTH = 25;
     private enum ERROR_TYPE {
@@ -32,6 +39,14 @@ public class UpdateScoresActivity extends Activity {
         this.playerScore = getIntent().getIntExtra("score", 0);
         TextView scoreDisplayer = findViewById(R.id.txv_score);
         scoreDisplayer.setText(scoreDisplayer.getText() + " " + this.playerScore);
+        displayWinOrLoss(true, true);
+        Timer overlayTimer = new Timer();
+        overlayTimer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                runOnUiThread(() -> displayWinOrLoss(false, true));
+            }
+        }, 5000);
     }
 
     public void btnSubmitOnClick(View view) {
@@ -54,7 +69,7 @@ public class UpdateScoresActivity extends Activity {
     }
 
     public void displayError(ERROR_TYPE error_type){
-        findViewById(R.id.imv_error_symbol).setBackgroundResource(R.mipmap.error_symbol);
+        findViewById(R.id.img_error_symbol).setBackgroundResource(R.mipmap.error_symbol);
         String errorMessage = "";
         switch (error_type) {
             case BLANK:
@@ -65,6 +80,16 @@ public class UpdateScoresActivity extends Activity {
                 break;
         }
         ((TextView) findViewById(R.id.txv_error_msg)).setText(errorMessage);
+    }
+
+    private void displayWinOrLoss(boolean display, boolean didWin){
+        ImageView tint = findViewById(R.id.img_tint);
+        Button submitButton = findViewById(R.id.btn_submit);
+        tint.setVisibility(display ? View.VISIBLE : View.INVISIBLE);
+        submitButton.setVisibility(display ? View.INVISIBLE : View.VISIBLE);
+        if (!display){
+            findViewById(R.id.gifImageView).setVisibility(View.INVISIBLE);
+        }
     }
 
 }
